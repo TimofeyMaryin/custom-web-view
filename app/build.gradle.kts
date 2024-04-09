7cfb6a1d0b5c210065ed3f828cd3fb3f366301d0
Timofey Maryin <nemotim4ic@gmail.com>
2024-04-09 11:00:35 +0500

    Fix TroubleSheet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "custom.test.view"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "custom.test.view"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
